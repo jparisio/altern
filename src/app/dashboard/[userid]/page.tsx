961b058ae22e5ffc160d474b9e041be315936a05
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-05-25 15:01:06 -0400

    fix attempt a million

--- src/app/dashboard/[userid]/page.tsx
+++ src/app/dashboard/[userid]/page.tsx
@@ -24,6 +24,8 @@ export default async function Page() {
   const playlistsData = await playlistsRes.json();
   const playlists: SpotifyPlaylist[] = playlistsData.items;
 
+  console.log(playlists);
+
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <div className="flex flex-col items-center justify-center mb-10">

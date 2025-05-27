d8c4b2d964d41988e701ee68335088408323d8f9
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-05-27 09:59:53 -0400

    attempting to grab all apple playlist tracks

--- src/app/api/export/apple-to-spotify/route.ts
+++ src/app/api/export/apple-to-spotify/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  let allTracks = [];
+  const allTracks = [];
   let url = tracksHref;
 
   // Example: fetch tracks (handle pagination if needed)

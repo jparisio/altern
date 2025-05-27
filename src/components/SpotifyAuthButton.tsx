bcecbefbecfd2612b0b2215903f140ab4d04ae37
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-05-27 12:34:13 -0400

    nice ui changes

--- src/components/SpotifyAuthButton.tsx
+++ src/components/SpotifyAuthButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 export default function SpotifyAuthButton() {
   const handleClick = () => {
     window.location.href = "/api/login";

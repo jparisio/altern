22b4833ca84c9d16407f4c8e7cfa6440bc2d9b0b
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-05-27 10:32:15 -0400

    test for tracks id instead now

---
+++ src/app/dashboard/export-status/page.tsx
@@ -0,0 +1,9 @@
+export default function ExportStatus() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-8">Success</h1>
+      </div>
+    </main>
+  );
+}

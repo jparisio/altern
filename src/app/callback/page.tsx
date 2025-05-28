b188ca4b9f2c0667da70ce608e516cc15026e9af
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-05-28 15:33:37 -0400

    code block

---
+++ src/app/callback/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Callback() {
+  useEffect(() => {
+    window.location.href = `/api/callback${window.location.search}`;
+  }, []);
+
+  return <p>Redirecting via Spotify...</p>;
+}
